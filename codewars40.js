0269fcd1320832a05e2ec0220cec9d7bdb39e658
D1Jax <jaxthoirs93@yahoo.co.uk>
2024-02-02 13:43:04 +0000

    finding the position of caps in a word

---
+++ codewars40.js
@@ -0,0 +1,10 @@
+var capitals = function (word) {
+	let arr=[]
+  
+  for(let i=0; i<word.length; i++){
+    if(word[i]===word[i].toUpperCase()){
+      arr.push(word.indexOf(word[i]))
+    }
+  }
+  return arr
+};
